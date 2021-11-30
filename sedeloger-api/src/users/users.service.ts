a42133badd250f89e2eabda56ef23d7daf438dba
jordanivan <jordanivan@hotmail.fr>
2021-11-30 01:15:03 +0100

    Update users.service.ts

--- sedeloger-api/src/users/users.service.ts
+++ sedeloger-api/src/users/users.service.ts
@@ -42,9 +42,6 @@ export class UsersService {
     async getUser(email_address: string): Promise<UsersEntity> {
         return await this.usersRepository.findOne({
             where: { email_address: email_address }
-
-
-            
         })
     }
 }
