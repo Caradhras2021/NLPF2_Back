5b8311ccabad0a929a0e28e5b24d52e6cf891c8a
jordanivan <jordanivan@hotmail.fr>
2021-11-30 01:13:59 +0100

    Update logs.service.ts

--- sedeloger-api/src/logs/logs.service.ts
+++ sedeloger-api/src/logs/logs.service.ts
@@ -11,9 +11,6 @@ export class LogsService {
     private usersRepository: Repository<UserEntity>,
   ) {}
 
-
-
-  
   async postLogsService(logs: Logs): Promise<UserEntity> {
     const currentdate = new Date().toLocaleString();
     try {
